a392e1f47e6ad255be0b1f37bb76f56608b7c462
Baas202 <b.pondman@gmail.com>
2016-11-10 14:27:41 +0100

    ukraine file added

---
+++ ukraine.py
@@ -0,0 +1,35 @@
+v0 = "cherkasy"
+v1 = "chernihiv"
+v2 = "chernivtsi"
+v3 = "dnipropetrovsk"
+v4 = "donetsk"
+v5 = "ivano-frankivsk"
+v6 = "kharkiv"
+v7 = "kherson"
+v8 = "khmelnytskyi"
+v9 = "kiev"
+v10 = "kirovohrad"
+v11 = "luhansk"
+v12 = "lviv"
+v13 = "mykolaiv"
+v14 = "odessa"
+v15 = "poltava"
+v16 = "rivne"
+v17 = "sumy"
+v18 = "ternopil"
+v19 = "vinnytsia"
+v20 = "volyn"
+v21 = "zakarpattia"
+v22 = "zaporizhia"
+v23 = "zhytomyr"
+v24 = "autonomous republic of crimea"
+v25 = "city of kiev"
+v26 = "city of sevastopol"
+
+oblasts = {v0: [v15, v9, v19, v10], v1: [v17, v15, v9], v2: [v5, v18, v8, v19], v3: [v6, v4, v22, v7, v13, v10, v15],
+           v4: [v11, v6, v3, v22], v5: [v21, v12, v18, v2], v6: [v17, v15, v3, v4, v11], v7: [v24, v22, v3, v13],
+           v8: [v16, v23, v19, v2, v18], v9: [v25, v1, v15, v0, v19, v23], v10: [v13, v14, v19, v0, v15, v3],
+           v11: [v6, v4], v12: [v20, v16, v18, v5, v21], v13: [v14, v10, v3, v7], v14: [v19, v10, v13],
+           v15: [v17, v6, v3, v10, v0, v9, v1], v16: [v20, v23, v8, v18, v12], v17: [v1, v15, v6],
+           v18: [v16, v8, v2, v5, v12], v19: [v23, v9, v0, v10, v14, v2, v8], v20: [v16, v12], v21: [v12, v5],
+           v22: [v4, v3, v7], v23: [v16, v8, v19, v9], v24: [v26, v7], v25: [v9], v26: [v24]}
